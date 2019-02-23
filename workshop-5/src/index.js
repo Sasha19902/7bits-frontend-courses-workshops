caff05da130ffcc278d81cbd5749fe0bfc4762d0
Sasha19902 <sasha19902@gmail.com>
2019-02-23 21:01:25 +0600

    workshop5

--- workshop-5/src/index.js
+++ workshop-5/src/index.js
@@ -4,19 +4,30 @@ import indexTemplate from './pages/index/index.hbs';
 import articleTemplate from './components/article/article.hbs';
 import spinnerTemplate from './components/spinner/spinner.hbs';
 
-const urls = [
+var urls = [
   'data1.json',
   'data2.json',
   'data3.json',
-  'data4.json'
+  'data4.json',
 ];
+
+
 
 document.addEventListener("DOMContentLoaded", function() {
   const root = $('#root');
   root.append(indexTemplate());
   const content = $('.content');
 
-  /**
-   * Place your code here
-   */
+    Promise.all(urls.map(url => fetch(`api/${url}`).then(elem => elem.json()).catch(function (message) {
+        console.log(message);
+    })))
+        .then(response => response.forEach(
+            element => {
+                if(element !== undefined)
+                    element.data.forEach(
+                        toPrint => content.append(articleTemplate(toPrint)))
+            }
+        ));
+
+
 });
